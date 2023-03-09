995dcfe3b45a3aae820f1e90a3597bb18ed698df
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2023-03-09 17:44:38 -0300

    Cria experimento 2 e atualiza funcoes

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def funcao_objetivo_cb(individuo):
+    """Computa a função objetivo no problema das caixas binárias.
+    
+    Args:
+      individiuo: lista contendo os genes das caixas binárias
+    
+    Return:
+      Um valor representando a soma dos genes do individuo.
+    """
+    return sum(individuo)
