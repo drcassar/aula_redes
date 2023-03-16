bcb6a4a61bc96f74bc3948fc491812e8620957c7
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2023-03-16 14:52:51 -0300

    atualiza funcoes.py e A.01

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,3 +1,45 @@
+import random
+
+
+def funcao_objetivo_cb(individuo):
+    """Computa a função objetivo no problema das caixas binárias.
+    
+    Args:
+      individiuo: lista contendo os genes das caixas binárias
+    
+    Return:
+      Um valor representando a soma dos genes do individuo.
+    """
+    return sum(individuo)
+
+
+def gene_cb():
+    """Gera um gene válido para o problema das caixas binárias
+    
+    Return:
+      Um valor zero ou um.
+    """
+    lista = [0, 1]
+    gene = random.choice(lista)
+    return gene
+
+
+def individuo_cb(n):
+    """Gera um individuo para o problema das caixas binárias.
+    
+    Args:
+      n: número de genes do indivíduo.
+    
+    Return:
+       Uma lista com n genes. Cada gene é um valor zero ou um.
+    """
+    individuo = []
+    for i in range(n):
+        gene = gene_cb()
+        individuo.append(gene)
+    return individuo
+
+
 def funcao_objetivo_cb(individuo):
     """Computa a função objetivo no problema das caixas binárias.
     
